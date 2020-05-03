ab5fc68788b4567af26d29a78b7ab7a90dd5e43e
Emad-Shtay <emad.shtay@yandex.ru>
2020-05-04 01:36:49 +0300

    final result

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const app = new Vue({
     el: '#app',
     // ========================
     data: {
+      title: 'TODOS',
+  todos: [
+    { text: 'todo 1', done: false, id: Date.now() },
+    { text: 'todo 2', done: false, id: Date.now() + 1 }
+  ]
      
     },
     // ========================
     methods: {
-      
-    
+      addTodo(event){
+        const text = event.target.value
+        this.todos.push({text,done:false,id:Date.now()})
+        event.target.value=''
+      },
+      removeTodo(id){
+        this.todos = this.todos.filter(todo =>todo.id !==id)
+      },
+
+      check(todo){
+        todo.done =!todo.done
+      }
     },
     // ========================
     computed: {
